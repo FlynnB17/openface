6f6da9c7439e234f73412cc84e78d25da0bd83ca
Flynn Bastin <FlynnB17@users.noreply.github.com>
2017-03-28 23:55:10 +0100

    Update classifier.py

--- demos/classifier.py
+++ demos/classifier.py
@@ -1,40 +1,14 @@
-#!/usr/bin/env python2
-#
-# Example to classify faces.
-# Brandon Amos
-# 2015/10/11
-#
-# Copyright 2015-2016 Carnegie Mellon University
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import time
-
 start = time.time()
-
 import argparse
 import cv2
 import os
 import pickle
-
 from operator import itemgetter
-
 import numpy as np
 np.set_printoptions(precision=2)
 import pandas as pd
-
 import openface
-
 from sklearn.pipeline import Pipeline
 from sklearn.lda import LDA
 from sklearn.preprocessing import LabelEncoder
@@ -43,7 +17,6 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.mixture import GMM
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
-
 fileDir = os.path.dirname(os.path.realpath(__file__))
 modelDir = os.path.join(fileDir, '..', 'models')
 dlibModelDir = os.path.join(modelDir, 'dlib')
@@ -115,47 +88,6 @@ def train(args):
 
     if args.classifier == 'LinearSvm':
         clf = SVC(C=1, kernel='linear', probability=True)
-    elif args.classifier == 'GridSearchSvm':
-        print("""
-        Warning: In our experiences, using a grid search over SVM hyper-parameters only
-        gives marginally better performance than a linear SVM with C=1 and
-        is not worth the extra computations of performing a grid search.
-        """)
-        param_grid = [
-            {'C': [1, 10, 100, 1000],
-             'kernel': ['linear']},
-            {'C': [1, 10, 100, 1000],
-             'gamma': [0.001, 0.0001],
-             'kernel': ['rbf']}
-        ]
-        clf = GridSearchCV(SVC(C=1, probability=True), param_grid, cv=5)
-    elif args.classifier == 'GMM':  # Doesn't work best
-        clf = GMM(n_components=nClasses)
-
-    # ref:
-    # http://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html#example-classification-plot-classifier-comparison-py
-    elif args.classifier == 'RadialSvm':  # Radial Basis Function kernel
-        # works better with C = 1 and gamma = 2
-        clf = SVC(C=1, kernel='rbf', probability=True, gamma=2)
-    elif args.classifier == 'DecisionTree':  # Doesn't work best
-        clf = DecisionTreeClassifier(max_depth=20)
-    elif args.classifier == 'GaussianNB':
-        clf = GaussianNB()
-
-    # ref: https://jessesw.com/Deep-Learning/
-    elif args.classifier == 'DBN':
-        from nolearn.dbn import DBN
-        clf = DBN([embeddings.shape[1], 500, labelsNum[-1:][0] + 1],  # i/p nodes, hidden nodes, o/p nodes
-                  learn_rates=0.3,
-                  # Smaller steps mean a possibly more accurate result, but the
-                  # training will take longer
-                  learn_rate_decays=0.9,
-                  # a factor the initial learning rate will be multiplied by
-                  # after each iteration of the training
-                  epochs=300,  # no of iternation
-                  # dropouts = 0.25, # Express the percentage of nodes that
-                  # will be randomly dropped as a decimal.
-                  verbose=1)
 
     if args.ldaDim > 0:
         clf_final = clf
